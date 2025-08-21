53adf7d2262073189de595567746347a81fc507c
Gresilda Hairstyle <gresilda@hairstyle.com>
2025-08-21 14:08:26 +0200

    Fix next.config.js for production

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: {}
-  },
   eslint: {
-    // Durante il build, ignora gli errori ESLint
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Durante il build, ignora gli errori TypeScript
     ignoreBuildErrors: true,
   },
 }
